865e80b1eb959370240f4742af938332499d90e2
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-05-13 14:57:03 -0300

    feat: adicionadas annotations e classe Endereco embedded

--- src/main/java/com/fatec/bluds/api/Instituicao/Endereco/Endereco.java
+++ src/main/java/com/fatec/bluds/api/Instituicao/Endereco/Endereco.java
@@ -1,4 +1,13 @@
 package com.fatec.bluds.api.Instituicao.Endereco;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
 }
--- src/main/java/com/fatec/bluds/api/Instituicao/InstituicaoEnsino.java
+++ src/main/java/com/fatec/bluds/api/Instituicao/InstituicaoEnsino.java
@@ -1,8 +1,8 @@
 package com.fatec.bluds.api.Instituicao;
 
 import com.fatec.bluds.api.Instituicao.Endereco.Endereco;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class InstituicaoEnsino {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    @NotNull
     private String nome;
+
+    @Column
+    @NotNull
     private String telefone;
+
+    @Column
+    @NotNull
     private String email;
+
+    @Embedded
     private Endereco endereco;
 }
