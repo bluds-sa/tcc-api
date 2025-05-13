c6ea8073b1f86d9cd38e6d4c116f1ec0a44e65f8
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-05-13 15:50:35 -0300

    feat: criada a classe Estudante

--- src/main/java/com/fatec/bluds/api/Usuario/Subclasses/Estudante/Estudante.java
+++ src/main/java/com/fatec/bluds/api/Usuario/Subclasses/Estudante/Estudante.java
@@ -1,4 +1,31 @@
 package com.fatec.bluds.api.Usuario.Subclasses.Estudante;
 
-public class Estudante {
+import com.fatec.bluds.api.Usuario.Subclasses.Estudante.Enums.AnoEscolar;
+import com.fatec.bluds.api.Usuario.Subclasses.Estudante.Enums.Periodo;
+import com.fatec.bluds.api.Usuario.Usuario;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Estudante")
+@Table(name = "Estudante")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Estudante extends Usuario {
+    @Column(unique = true)
+    @NotNull
+    private String matricula;
+
+    @Column
+    @NotNull
+    private Periodo periodo;
+
+    @Column
+    @NotNull
+    private AnoEscolar anoEscolar;
 }
