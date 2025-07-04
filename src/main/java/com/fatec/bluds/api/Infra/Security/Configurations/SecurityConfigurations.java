ee6106cdafee1ff27d68a61bca3741941ae235d3
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-07-04 10:39:26 -0300

    feat: alteradas configurações de segurança que impediam o console do H2 Database se ser visualizado

--- src/main/java/com/fatec/bluds/api/Infra/Security/Configurations/SecurityConfigurations.java
+++ src/main/java/com/fatec/bluds/api/Infra/Security/Configurations/SecurityConfigurations.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +25,8 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll())
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .build();
     }
 
