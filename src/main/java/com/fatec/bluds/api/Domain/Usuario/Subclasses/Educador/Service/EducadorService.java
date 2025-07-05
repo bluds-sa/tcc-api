ae0cb9ad93c3c9d9d8bfba680038148be9b6933e
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-07-05 10:22:40 -0300

    feat: criado EducadorService

--- src/main/java/com/fatec/bluds/api/Domain/Usuario/Subclasses/Educador/Service/EducadorService.java
+++ src/main/java/com/fatec/bluds/api/Domain/Usuario/Subclasses/Educador/Service/EducadorService.java
@@ -1,4 +1,46 @@
 package com.fatec.bluds.api.Domain.Usuario.Subclasses.Educador.Service;
 
-public class EducadorService {
+import com.fatec.bluds.api.Domain.Usuario.Service.UsuarioServiceInterface;
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Educador.Educador;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EducadorService implements UsuarioServiceInterface<Educador> {
+
+
+    @Override
+    public Educador create(Educador user) {
+        return null;
+    }
+
+    @Override
+    public Optional<Educador> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Educador> findByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Educador> findByMatricula(String matricula) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Educador> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Educador update(Educador user) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
