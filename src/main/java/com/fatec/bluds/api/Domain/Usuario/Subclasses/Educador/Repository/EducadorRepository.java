1e63d0d34849d284cae57c1db4d00113db63bd73
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-07-05 10:50:36 -0300

    feat: criado EducadorRepository

---
+++ src/main/java/com/fatec/bluds/api/Domain/Usuario/Subclasses/Educador/Repository/EducadorRepository.java
@@ -0,0 +1,6 @@
+package com.fatec.bluds.api.Domain.Usuario.Subclasses.Educador.Repository;
+
+import com.fatec.bluds.api.Domain.Usuario.Repository.UsuarioRepository;
+
+public interface EducadorRepository extends UsuarioRepository {
+}
