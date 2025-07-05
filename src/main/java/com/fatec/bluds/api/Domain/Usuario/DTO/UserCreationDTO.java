a0078d8c46d216c15370bccf1764356b43e1d296
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-07-05 09:57:13 -0300

    fix: correção na documentação da classe UsuarioCreationDTO

--- src/main/java/com/fatec/bluds/api/Domain/Usuario/DTO/UserCreationDTO.java
+++ src/main/java/com/fatec/bluds/api/Domain/Usuario/DTO/UserCreationDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
-/** DTO criado com o objetivo de conter os dados básicos necessários para a criação de um usuário, independentemente da subclasse **/
+/** DTO criado para conter os dados básicos necessários para a criação de um usuário, independentemente da subclasse **/
 public record UserCreationDTO(
         @NotBlank
         String nome,
