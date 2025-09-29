ecbf12be8ace613ea65b926c143ef662f84f7f14
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-09-29 17:48:04 -0300

    feat: criar UserFactory

--- src/main/java/com/fatec/bluds/api/Domain/Usuario/Factory/UserFactory.java
+++ src/main/java/com/fatec/bluds/api/Domain/Usuario/Factory/UserFactory.java
@@ -1,4 +1,64 @@
 package com.fatec.bluds.api.Domain.Usuario.Factory;
 
+import com.fatec.bluds.api.Domain.Usuario.DTO.RegisterDTO;
+import com.fatec.bluds.api.Domain.Usuario.Roles.Roles;
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Educador.Educador;
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Estudante.Estudante;
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.Gestor;
+import com.fatec.bluds.api.Domain.Usuario.Usuario;
+
 public class UserFactory {
+
+    public static Usuario createUser(RegisterDTO dto) {
+        switch (dto.userType()) {
+            case ESTUDANTE -> {
+                Estudante estudante = new Estudante();
+                estudante.setNome(dto.nome());
+                estudante.setCpf(dto.cpf());
+                estudante.setEmail(dto.email());
+                estudante.setTelefone(dto.telefone());
+                estudante.setGenero(dto.genero());
+                estudante.setDataNascimento(dto.dataNascimento());
+                estudante.setInstituicaoEnsino(dto.instituicaoEnsino());
+                estudante.setSenha(dto.senha());
+                estudante.setRoles(Roles.ESTUDANTE);
+                estudante.setMatricula(dto.matricula());
+                estudante.setPeriodo(dto.periodo());
+                estudante.setAnoEscolar(dto.anoEscolar());
+
+                return estudante;
+            }
+            case EDUCADOR -> {
+                Educador educador = new Educador();
+                educador.setNome(dto.nome());
+                educador.setCpf(dto.cpf());
+                educador.setEmail(dto.email());
+                educador.setTelefone(dto.telefone());
+                educador.setGenero(dto.genero());
+                educador.setDataNascimento(dto.dataNascimento());
+                educador.setInstituicaoEnsino(dto.instituicaoEnsino());
+                educador.setSenha(dto.senha());
+                educador.setRoles(Roles.EDUCADOR);
+                educador.setTitulo(dto.titulo());
+
+                return educador;
+            }
+            case GESTOR -> {
+                Gestor gestor = new Gestor();
+                gestor.setNome(dto.nome());
+                gestor.setCpf(dto.cpf());
+                gestor.setEmail(dto.email());
+                gestor.setTelefone(dto.telefone());
+                gestor.setGenero(dto.genero());
+                gestor.setDataNascimento(dto.dataNascimento());
+                gestor.setInstituicaoEnsino(dto.instituicaoEnsino());
+                gestor.setSenha(dto.senha());
+                gestor.setRoles(Roles.GESTOR);
+                gestor.setCargo(dto.cargo());
+
+                return gestor;
+            }
+            default -> throw new IllegalArgumentException("Tipo de usuário inválido: " + dto.userType());
+        }
+    }
 }
