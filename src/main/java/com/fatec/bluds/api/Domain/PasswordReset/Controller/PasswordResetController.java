51387617ea59da8400858c7b4548b2dde516665d
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-06 03:26:42 -0300

    docs(PasswordResetController): adicionar documentação dos endpoints

--- src/main/java/com/fatec/bluds/api/Domain/PasswordReset/Controller/PasswordResetController.java
+++ src/main/java/com/fatec/bluds/api/Domain/PasswordReset/Controller/PasswordResetController.java
@@ -3,6 +3,10 @@ package com.fatec.bluds.api.Domain.PasswordReset.Controller;
 import com.fatec.bluds.api.Domain.PasswordReset.DTO.PasswordResetDTO;
 import com.fatec.bluds.api.Domain.PasswordReset.DTO.RequestResetDTO;
 import com.fatec.bluds.api.Domain.PasswordReset.Service.PasswordResetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +15,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reset-password")
+@Tag(name = "Redefinição de Senha", description = "Responsável por solicitar e efetuar a redefinição da senha do usuáro")
 public class PasswordResetController {
 
     @Autowired
     private PasswordResetService passwordResetService;
 
+    @Operation(summary = "Solicita redefinição de senha através do e-mail", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",  description = "Caso o e-mail existir, será enviado um link com token de redefinição"),
+    })
     @PostMapping("/forgot-password")
     public ResponseEntity<Object> forgotPassword(@RequestBody @Valid RequestResetDTO dto) throws MessagingException {
         passwordResetService.processRequest(dto);
@@ -23,6 +32,11 @@ public class PasswordResetController {
         return ResponseEntity.ok().body("Se o e-mail estiver cadastrado, você receberá instruções de redefinição de senha.");
     }
 
+    @Operation(summary = "Valida o token enviado através do email", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",  description = "O token é válido"),
+            @ApiResponse(responseCode = "400",  description = "O token não é válido")
+    })
     @PostMapping("/validate")
     public ResponseEntity<Object> validateToken(@RequestParam String token) {
         if (passwordResetService.validateToken(token)) {
@@ -32,6 +46,11 @@ public class PasswordResetController {
         }
     }
 
+    @Operation(summary = "Recebe o token e senha nova do usuário para redefiní-la", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",  description = "A senha é alterada com sucesso"),
+            @ApiResponse(responseCode = "400",  description = "Não foi possível atualizar a senha")
+    })
     @PutMapping()
     public ResponseEntity<Object> updatePassword(@RequestBody PasswordResetDTO dto) {
         if (passwordResetService.updatePassword(dto)) {
