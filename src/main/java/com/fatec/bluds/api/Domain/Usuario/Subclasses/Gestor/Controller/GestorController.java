cbf7a243a8c8b5134167fd9c10e4a177f90b5753
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-07 03:30:45 -0300

    feat: buscarGestorByID

--- src/main/java/com/fatec/bluds/api/Domain/Usuario/Subclasses/Gestor/Controller/GestorController.java
+++ src/main/java/com/fatec/bluds/api/Domain/Usuario/Subclasses/Gestor/Controller/GestorController.java
@@ -1,6 +1,13 @@
 package com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.Controller;
 
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.DTO.GestorDetailsDTO;
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.Gestor;
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.Service.GestorService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/gestor")
 @Tag(name = "Módulo de Gestores", description = "Lida com operações relacionadas à classe Gestor")
 public class GestorController {
+
+    @Autowired
+    private GestorService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getGestorById(@PathVariable Long id) {
+        Gestor gestor = service.getGestorById(id);
+
+        return ResponseEntity.ok(new GestorDetailsDTO(gestor));
+    }
 }
--- src/main/java/com/fatec/bluds/api/Domain/Usuario/Subclasses/Gestor/Service/GestorService.java
+++ src/main/java/com/fatec/bluds/api/Domain/Usuario/Subclasses/Gestor/Service/GestorService.java
@@ -1,7 +1,24 @@
 package com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.Service;
 
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.Gestor;
+import com.fatec.bluds.api.Domain.Usuario.Subclasses.Gestor.Repository.GestorRepository;
+import com.fatec.bluds.api.Infra.Exceptions.Usuario.UsuarioNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GestorService {
+
+    @Autowired
+    private GestorRepository repository;
+
+    public Gestor getGestorById(Long id) {
+        Optional<Gestor> optionalGestor = repository.findById(id);
+
+        return optionalGestor.orElseThrow(
+                () -> new UsuarioNotFoundException("Gestor com ID " + id + " não foi encontrado")
+        );
+    }
 }
