12f5d159d9e7e695d240ae4855277f167570a79a
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-30 00:01:03 -0300

    fix: corrigir RequestMapping do controller de disciplinas

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/controller/DisciplinaController.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/controller/DisciplinaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/instituicoes/{idInstituicao}/disciplinas")
+@RequestMapping("/disciplinas")
 @Tag(name = "Módulo Disciplinas", description = "Responsável por operações relacionadas a Disciplinas dentro de uma Instiuição de Ensino")
 public class DisciplinaController {
 
