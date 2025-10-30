61c82276bebf5a717808f0f5ba1a8179e198206e
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-30 02:31:13 -0300

    feat: criar service que implementa DisciplinaService

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaServiceImpl.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaServiceImpl.java
@@ -1,4 +1,56 @@
 package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.service;
 
-public class DisciplinaServiceImpl {
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.dto.*;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.model.Disciplina;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.repository.DisciplinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DisciplinaServiceImpl implements DisciplinaService{
+
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+
+    @Override
+    public Disciplina createDisciplina(CreateDisciplinaDTO dto) {
+        return null;
+    }
+
+    @Override
+    public List<Disciplina> getDisciplinasByEstudante(DisciplinaByEstudanteDTO dto) {
+        return List.of();
+    }
+
+    @Override
+    public List<Disciplina> getDisciplinasByInstituicao(DisciplinaByInstituicaoDTO dto) {
+        return List.of();
+    }
+
+    @Override
+    public Disciplina updateDisciplina(UpdateDisciplinaDTO dto) {
+        return null;
+    }
+
+    @Override
+    public DisciplinaEstudantesDTO addEstudanteToDisciplina(DisciplinaByEstudanteDTO dto) {
+        return null;
+    }
+
+    @Override
+    public DisciplinaEstudantesDTO removeEstudanteFromDisciplina(DisciplinaByEstudanteDTO dto) {
+        return null;
+    }
+
+    @Override
+    public DisciplinaEstudantesDTO enrollByBulk(EstudantesByBulkDTO dto) {
+        return null;
+    }
+
+    @Override
+    public DisciplinaEstudantesDTO unenrollByBulk(EstudantesByBulkDTO dto) {
+        return null;
+    }
 }
