3fa49e01d13e1c8a450f207b5b492413a1d3ff10
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-30 00:32:22 -0300

    fix: ponto e vírgula faltando

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface DisciplinaService {
     Disciplina createDisciplina(CreateDisciplinaDTO dto);
     List<Disciplina> getDisciplinasByEstudante(ListDisciplinaByEstudanteDTO dto);
-    List<Disciplina> getDisciplinasByInstituicao(ListDisciplinaByInstituicaoDTO dto)
+    List<Disciplina> getDisciplinasByInstituicao(ListDisciplinaByInstituicaoDTO dto);
     Disciplina updateDisciplina(UpdateDisciplinaDTO dto);
 }
