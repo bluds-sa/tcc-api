b6481bcf1f19a51993a0e2b65a01b31666b1ee1e
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-30 00:31:45 -0300

    feat: criar método e DTO de buscar Disciplinas por Instituição

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
@@ -2,6 +2,7 @@ package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.service;
 
 import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.dto.CreateDisciplinaDTO;
 import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.dto.ListDisciplinaByEstudanteDTO;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.dto.ListDisciplinaByInstituicaoDTO;
 import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.dto.UpdateDisciplinaDTO;
 import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.model.Disciplina;
 
@@ -10,5 +11,6 @@ import java.util.List;
 public interface DisciplinaService {
     Disciplina createDisciplina(CreateDisciplinaDTO dto);
     List<Disciplina> getDisciplinasByEstudante(ListDisciplinaByEstudanteDTO dto);
+    List<Disciplina> getDisciplinasByInstituicao(ListDisciplinaByInstituicaoDTO dto)
     Disciplina updateDisciplina(UpdateDisciplinaDTO dto);
 }
