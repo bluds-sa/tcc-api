2b1ea5d6e5f0b825e8e8f64b72c503555d40dd59
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-30 00:19:21 -0300

    feat: criar método e DTO de criação de Disciplinas

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
@@ -1,4 +1,8 @@
 package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.service;
 
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.dto.CreateDisciplinaDTO;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.model.Disciplina;
+
 public interface DisciplinaService {
+    Disciplina createDisciplina(CreateDisciplinaDTO dto);
 }
