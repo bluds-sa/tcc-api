9d7fcc4451471cbf08b3fc5abd99fe7202be603e
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-30 17:14:21 -0300

    feat: criar método getDisciplinasByEducador

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/service/DisciplinaService.java
@@ -11,6 +11,7 @@ public interface DisciplinaService {
     Disciplina getDisciplinaById(Long id);
     List<Disciplina> getDisciplinasByEstudante(DisciplinaByEstudanteDTO dto);
     List<Disciplina> getDisciplinasByInstituicao(DisciplinaByInstituicaoDTO dto);
+    List<Disciplina> getDisciplinasByEducador(DisciplinaByEducadorDTO dto);
     Disciplina updateDisciplina(Long id,UpdateDisciplinaDTO dto);
     List<Estudante> getEstudantesFromDisciplina(Long id);
     List<Estudante> addEstudanteToDisciplina(DisciplinaByEstudanteDTO dto);
