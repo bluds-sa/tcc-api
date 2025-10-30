d9e3696d6d1941757ac93e33b500e86e0bfbe0e0
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-30 18:22:26 -0300

    chore: deletar DTOs inutilizados

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/dto/DisciplinaByInstituicaoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record DisciplinaByInstituicaoDTO(
-        @NotNull
-        Long instituicaoId
-) {
-}
