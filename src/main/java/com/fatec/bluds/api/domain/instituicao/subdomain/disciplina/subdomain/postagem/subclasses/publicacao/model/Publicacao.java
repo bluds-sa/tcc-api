b3e51b3a761635d40ed3e17cb6075e302aefcd05
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-06 21:58:05 -0300

    feat: adicionar EqualsAndHashCode em Postagens

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/publicacao/model/Publicacao.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/publicacao/model/Publicacao.java
@@ -1,6 +1,10 @@
 package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.model;
 
 import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.model.Postagem;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 
+@Entity(name = "tarefa")
+@DiscriminatorValue("TAREFA")
 public class Publicacao extends Postagem {
 }
