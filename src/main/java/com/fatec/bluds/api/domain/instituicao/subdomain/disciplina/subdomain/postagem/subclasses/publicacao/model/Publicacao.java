fba2124a2addb40dcb839750b3b1c8040105e7c7
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-06 21:47:34 -0300

    feat: criar classe Publicacao

---
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/publicacao/model/Publicacao.java
@@ -0,0 +1,6 @@
+package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.model;
+
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.model.Postagem;
+
+public class Publicacao extends Postagem {
+}
