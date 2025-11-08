7fa1e3d36b4ad48110255ab1d830b17c6db93101
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-08 00:55:45 -0300

    feat: adicionar tarefaService ao TarefaController

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/tarefa/controller/TarefaController.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/tarefa/controller/TarefaController.java
@@ -1,6 +1,8 @@
 package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.tarefa.controller;
 
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.tarefa.service.TarefaService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tarefas")
 @Tag(name = "Módulo Tarefas", description = "Responsável por operações relacionadas a Tarefas dentro de uma Disciplina")
 public class TarefaController {
+
+    @Autowired
+    private final TarefaService tarefaService;
+
+    public TarefaController(TarefaService tarefaService) {
+        this.tarefaService = tarefaService;
+    }
+
+    
 }
