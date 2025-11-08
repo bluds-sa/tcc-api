97d4224b3d00aea1222e85dcca036324e75cfd49
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-07 23:35:23 -0300

    feat: criar PublicacaoController

---
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/publicacao/controller/PublicacaoController.java
@@ -0,0 +1,24 @@
+package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.controller;
+
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.service.PublicacaoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publicacoes")
+@Tag(name = "Módulo Disciplinas", description = "Responsável por operações relacionadas a Disciplinas dentro de uma Instiuição de Ensino")
+public class PublicacaoController {
+
+    @Autowired
+    private final PublicacaoService publicacaoService;
+
+    public PublicacaoController(PublicacaoService publicacaoService) {
+        this.publicacaoService = publicacaoService;
+    }
+
+    
+}
