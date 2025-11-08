d7e0f6ad876aaf3d4bdcbd215ef077f2231e8722
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-08 15:54:09 -0300

    feat: implementar funcionalidades de Controller

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/publicacao/controller/PublicacaoController.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/publicacao/controller/PublicacaoController.java
@@ -1,10 +1,20 @@
 package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.controller;
 
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.dto.CreatePublicacaoDTO;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.dto.PublicacaoDetailsDTO;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.dto.PublicacaoSummaryDTO;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.dto.UpdatePublicacaoDTO;
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.model.Publicacao;
 import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.publicacao.service.PublicacaoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/publicacoes")
@@ -17,5 +27,48 @@ public class PublicacaoController {
         this.publicacaoService = publicacaoService;
     }
 
+    @PreAuthorize("hasRole('EDUCADOR')")
+    @PostMapping
+    public ResponseEntity<Object> createPublicacao(@RequestBody @Valid CreatePublicacaoDTO dto) {
+        Publicacao publicacao = publicacaoService.createPublicacao(dto);
 
+        return ResponseEntity.ok().body(new PublicacaoSummaryDTO(publicacao));
+    }
+
+    @GetMapping("/disciplina")
+    public ResponseEntity<Object> getPublicacoes(
+            @RequestParam
+            @NotNull(message = "ID da Disciplina não pode ser nulo")
+            @Positive(message = "ID da Disciplina não pode ser negativo ou zero")
+            Long discplinaId
+    ) {
+        List<Publicacao> publicacoes = publicacaoService.getPublicacoesByDisciplina(discplinaId);
+
+        return publicacoes.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok().body(
+                publicacoes.stream().map(PublicacaoDetailsDTO::new).toList()
+        );
+    }
+
+    @GetMapping("/{publicacaoId}")
+    public ResponseEntity<Object> getPublicacao(@PathVariable Long publicacaoId) {
+        Publicacao publicacao = publicacaoService.getPublicacaoById(publicacaoId);
+
+        return ResponseEntity.ok().body(new PublicacaoDetailsDTO(publicacao));
+    }
+
+    @PreAuthorize("hasRole('EDUCADOR')")
+    @PutMapping
+    public ResponseEntity<Object> updatePublicacao(@PathVariable Long publicacaoId, @RequestBody UpdatePublicacaoDTO dto) {
+        Publicacao publicacao = publicacaoService.updatePublicacao(publicacaoId, dto);
+
+        return ResponseEntity.ok().body(new PublicacaoDetailsDTO(publicacao));
+    }
+
+    @PreAuthorize("hasRole('EDUCADOR')")
+    @DeleteMapping("/{publicacaoId}")
+    public ResponseEntity<Object> deletePublicacao(@PathVariable Long publicacaoId) {
+        publicacaoService.removePublicacao(publicacaoId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
