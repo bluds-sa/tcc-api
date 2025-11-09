6685dae9b33aab5b1dd068bf2b6e4373df1ea73a
vitormdoja04 <vitor.doja@gmail.com>
2025-11-09 00:23:55 -0300

    feat: adicionar os arquivos DTO de Tarefas

--- src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/tarefa/dto/SummaryTarefaDTO.java
+++ src/main/java/com/fatec/bluds/api/domain/instituicao/subdomain/disciplina/subdomain/postagem/subclasses/tarefa/dto/SummaryTarefaDTO.java
@@ -1,4 +1,30 @@
 package com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.tarefa.dto;
 
-public class SummaryTarefaDTO {
+import com.fatec.bluds.api.domain.instituicao.subdomain.disciplina.subdomain.postagem.subclasses.tarefa.model.Tarefa;
+import java.time.LocalDateTime;
+
+public record SummaryTarefaDTO(
+        Long id,
+        String titulo,
+        String resumoConteudo,
+        LocalDateTime dataCriacao,
+        LocalDateTime dataExpiracao,
+        Double valorTotal,
+        String educadorResponsavel,
+        Long disciplinaId
+) {
+    public SummaryTarefaDTO(Tarefa t) {
+        this(
+                t.getId(),
+                t.getTitulo(),
+                t.getConteudo() != null && t.getConteudo().length() > 120
+                        ? t.getConteudo().substring(0, 120) + "..."
+                        : t.getConteudo(),
+                t.getDataEnvio(),
+                t.getDataExpiracao(),
+                t.getValorTotal(),
+                t.getAutor() != null ? t.getAutor().getNome() : null,
+                t.getDisciplina() != null ? t.getDisciplina().getId() : null
+        );
+    }
 }
